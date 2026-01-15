c54d804fb22d5757a414947c912fb0e0bc001932
Thronetaker <parmanand224radha@gmail.com>
2026-01-15 23:29:01 +0530

    userdata

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,7 +2,35 @@ import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import Profile from "../models/profile.model.js";
 import crypto from 'crypto';
+import PDFDocument from 'pdfkit';
+import fs from 'fs';
 
+const convertUserDataTOPDF = (userData) => {
+    const doc = new PDFDocument();
+
+    const outputPath = crypto.randomBytes(32).toString("hex");
+    const stream = fs.createWriteStream("uploads/"+ outputPath);
+
+    doc.pipe(stream);
+
+    doc.image(`uploads/${userData.userId.profilePicture}`,{align:"center",width: 100});
+    doc.fontSize(14).text(`Name : ${userData.userId.name}`);
+    doc.fontSize(14).text("Username: ${userData.userId.username}");
+    doc.fontSize(14).text(`Email: ${userData.userId.email});
+    doc.fontSize(14).text(Bio: $(userData.bio}");
+    doc.fontSize(14).text(Current Position: ${userData.currentPosition}`)
+   doc.fontSize(14).text("Past Work: ") 
+     userData.pastWork.forEach((work, index) => {
+        doc.fontSize(14).text(`Company Name: ${work.companyName}`);
+        doc.fontSize(14).text(`Position: ${work.position}`);
+        doc.fontSize(14).text(`Years: ${work.years}`);
+    })
+
+    doc.end();
+
+    return outputPath;
+
+}
 
 
 export const register = async(req, res) => {
@@ -116,5 +144,53 @@ export const getUserandProfile = async(req, res) => {
 
     }catch(err){
         return res.status(500).json({message: err.message});
+    }
+}
+
+export const updateProfileData = async(res, req) => {
+          try{
+            const {token, ...newProfileData} = req.body;
+
+            const userProfile = await User.findOne({token: token});
+            if(!userProfile) {
+            return res.status(404).json({message : " user not found!"});
+            }
+
+            const profile_to_update = await Profile.findOne({userId: userProfile._id}) ;
+
+            Object.assign(profile_to_update,newProfileData);
+
+            await profile_to_update.save();
+
+            return res.join({ message : "Profile Updated"});
+
+          }catch(error) {
+            return res.status(500).json({message: error.message});
+
+          }
+}
+
+export const getAllUserProfile = async(req,res) => {
+    try{
+        const profiles = await Profile.find().populate('userId',' name username email profilePicture');
+
+        return res.json({ profiles});
+
+    }catch(error){
+        return res.status(500).json({message : error.message})
+    }
+}
+
+export const downloadProfile = async(req, res) => {
+    const user_id = req.query.id;
+    const userProfile = await Profile.findOne( { userId : user_id})
+    .populate('userId', 'name username email profilPicture');
+
+    let a = await convertUserDataTOPDF(userProfile);
+    return res.json({ "message": a});
+    try{
+
+    }catch(error){
+        return res.status(500).json({message : error.message})
     }
 }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router } from "express";
-import { register , login, uploadProfilePicture, updateUserProfile,getUserandProfile} from "../controllers/user.controller.js";
+import { register , login, uploadProfilePicture,updateProfileData, updateUserProfile,getUserandProfile, getAllUserProfile} from "../controllers/user.controller.js";
 import multer from "multer";
 const router  = Router();
 
@@ -22,5 +22,8 @@ router.route('/login').post(login);
 router.route("/user_update").post(updateUserProfile);
 
 router.route('/get_user_and_profile').get(getUserandProfile);
+router.route('/update_profile_data').post(updateProfileData);
+router.route("/user/get_all_users").get(getAllUserProfile);
+router.route("/user/download_resume")
 
 export default router;
