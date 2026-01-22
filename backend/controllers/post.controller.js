a241f4c9c792abec3bf04ead6ed470bac316bb86
Thronetaker <parmanand224radha@gmail.com>
2026-01-22 08:56:52 +0530

    Multer post implement

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,5 @@
-
+import Post from "../models/posts.model.js";
+import User from "../models/user.model.js";
 
 
 export const activeCheck = async(req, res)  => {
@@ -6,3 +7,24 @@ export const activeCheck = async(req, res)  => {
 }
 
 
+export const createPost = async (req, res) => {
+    const {token} = req.body;
+    try{
+        const user = await User.findOne({token:token});
+        if(!user) return res.status(404).json({message : "User does not exists"}) ;
+
+        const post = new Post({
+            userId :user._id,
+            body: req.body.body,
+            media: req.file != undefined ? req.file.filename : "",
+            fileType: req.file != undefined ? req.file.mimetype.split("/")[1] : ""
+        })
+
+        await post.save();
+
+        return res.status(200).json({ message : "Post Created"})
+
+    }catch(err){
+        return res.status(200).json({message : err.message })
+    }
+}
