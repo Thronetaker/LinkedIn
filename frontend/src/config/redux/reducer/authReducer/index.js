d60a6f0c0c9e4a60cc01fd1d2c3dd696f04de649
Thronetaker <parmanand224radha@gmail.com>
2026-01-23 21:43:04 +0530

    F Authentication

---
+++ frontend/src/config/redux/reducer/authReducer/index.js
@@ -0,0 +1,51 @@
+const { createSlice } = require("@reduxjs/toolkit");
+const { connect } = require("react-redux");
+
+const initialState = {
+    user:[],
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    loggedIn: false,
+    message: '',
+    profilefetched: false,
+    connections: [],
+    connectionRequest: [],
+
+
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        reset: (state) => initialState,
+        handleLoginUser : (state) => {
+            state.message = "Login Successful";
+        }
+    },
+
+    extraReducers: (builder) => {
+        // Add Async Thunks Here
+        builder
+        .addCase(loginUser.pending, (state) => {
+            state.isLoading = true;
+            state.message = "Knocking the door "
+        })
+        .addCase(loginUser.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+            state.loggedIn = true;
+            state.message = "Login Successful";
+        })
+        .addCase(loginUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.loggedIn = false;
+            state.message = action.payload;
+            // .message || "Login Failed";
+        }  )    
+    }
+})
+
+export default authSlice.reducer;
